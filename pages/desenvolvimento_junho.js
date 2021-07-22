71b2d3b169a14f847ec76863ddf7ffda3c93e192
marciotakashi <marciotakashi.u@gmail.com>
2021-07-22 18:40:31 -0300

    pagina desenvolvimento_junho

---
+++ pages/desenvolvimento_junho.js
@@ -0,0 +1,55 @@
+function Desenvolvimento_Junho(){
+    return (
+        <body>
+            <h1>[Tô Off] Desenvolvimento Junho - Takashi</h1>
+            <h2>Introdução</h2>
+            <p> Hoje passei o dia estudando Web Scraping, e foi engraçado que a ideia surgiu da sprint de pessoas, pois era um assunto que eu sempre tive interesse, mas nunca tinh buscado saber um pouco mais sobre...</p>
+            <p> O que estou compartilhando com vocês aqui hoje, é um conjunto de:</p>
+            <ul>
+                <li>Pesquisas no google;</li>
+                <li>Vídeos disponíveis no youtube;</li>
+                <li>Algumas horas de estudo;</li>
+                <li>Parte de um curso que eu havia adquirido com auxílio desenvolvimento: "automate the boring stuff with python";</li>
+                <li>Aplicação de algumas coisas que eu havia estudado de Python para fazer uma iniciação científica na época da faculdade</li>
+            </ul>
+            <h2>O que eu aprendi?</h2>
+            <ul>
+                <li>Um pouco de HTML;</li>
+                <li>Um pouco de Web Scrapping;</li>
+                <li>Fazer meu primeiro programinha de Web Scraping (para um dev de verdade, nada de mais, para um mero explorador, uma grande conquista kkkkk) </li>
+            </ul>
+            <h2>Compartilhando o aprendizado!</h2>
+            <h3>O que é Web Scraping?</h3>
+            <p> De forma simplificada, Web Scraping é o processo de buscar, raspar, pegar informações disponíveis em uma página Web.</p>
+            <p> Se vocês clicarem com o botão direito do mouse em uma página da internet e ir em "inspect", você consegue ver um monte de código que "da forma" para o site que você está. Com o Web Scraping, você faz uma "mineiração" nesses códigos para achar o "ouro escondido" que seriam as informações que às vezes precisamos copiar manualmente ou que ficaria até difícil de localizar na página...</p>
+            <p> Por exemplo, para eu inserir essa foto no documento, eu peguei um link que está dentro da página pública do meu github.</p>
+            <img width="200px" src="https://avatars.githubusercontent.com/u/84753586?v=4" alt="Imagem do github"></img>
+            <p> Se vocês tiverem curiosidade, basta entrar no meu github, clickar com botão direito na minha foto, ir em inspect e você vai ver um texto mais ou menos assim sublinhado na tela: https://avatars.githubusercontent.com/u/84753586?v=4</p>
+            <a target="_blank" href="https://github.com/marciotakashi">(Acesse aqui meu github.)</a>
+            <p> Legal né? Bom, pelo menos eu acho kkkkkkk</p>
+            <h3>Web Scraping é uma prática ilegal?</h3>
+            <p> Não achei muita coisa conclusiva sobre isso, haviam muitas coisas falando que não era ilegal, mas o principal ponto aqui é sempre olhar a política de privacidade do site em questão (Não olhei se o github tem alguma restrição hehe)</p>
+            <h3>Possíveis usos:</h3>
+            <p> Pegar as informações de vários produtos em um e-commerce;</p>
+            <p> Pegar informações de usuários do github (é o que tentei fazer aqui hehe)</p>
+            <p> Monitorar um site de ações, cotações, promoções...</p>
+            <p> Como tudo na vida, nossa imaginação é o limite !</p>
+            <h3>O HTML:</h3>
+            <p> Para entender um pouco melhor sobre como o Web Scraping funcionava, precisei estudar o básico (muito básico mesmo) do HTML, mas basicamente ele trabalha com marcadores que sempre são apresentados entre o sinal de Menor e Maior e e são fechados com sinal de Menor, Barra e Maior (ainda não aprendi a digitar os marcadores aqui no HTML sem ele entender que é um marcador kkk e por isso escrevi por extenso)"</p>
+            <h3>Linguagens e bibliotecas:</h3>
+            <ul>
+                <li>Python;</li>
+                <li>BeautfulSoap;</li>
+                <li>Playwright;</li>
+                <li>Requests</li>
+            </ul>
+            <h3>Vídeo: </h3>
+            <p> Fiz um vídeo rapidinho compartilhando o que eu fiz e como ele funciona (De um modo leigo para leigos é claro, para desenvolvedores, talvez eu seja linchado por isso kkk)</p>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/UTJsCrIsPsk" frameborder="0" allowfullscreen></iframe>
+        </body>
+
+    )
+}
+
+
+export default Desenvolvimento_Junho

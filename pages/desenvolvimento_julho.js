c2c33837f479984369ff49f96cb2695fc8f3d9ce
marciotakashi <marciotakashi.u@gmail.com>
2021-07-22 20:27:11 -0300

    ajustes pagina desenvolvimento_julho

--- pages/desenvolvimento_julho.js
+++ pages/desenvolvimento_julho.js
@@ -2,7 +2,7 @@ function Desenvolvimento_Julho(){
     return (
         <body>
             <h1>[Tô Off] Desenvolvimento Julho - Takashi</h1>
-            <iframe width="1000" height="500" src="https://www.youtube.com/embed/WW9g3QeCrjI" frameborder="0" allowfullscreen></iframe>
+            <iframe src="https://www.youtube.com/embed/WW9g3QeCrjI" allowfullscreen></iframe>
         </body>
 
     )
